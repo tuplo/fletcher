37ba57e29d18cd954ed54a2323e6f7048a85a345
Rui Costa <ruicosta042@gmail.com>
2021-05-12 19:18:42 +0100

    feat: drop node10, ES bundle only

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,5 @@
 
 rimraf dist
 tsc --build tsconfig.build.json
-esbuild src/index.ts --bundle --platform=node --outfile=dist/index.js
-esbuild src/index.ts --bundle --outfile=dist/index.modern.js --external:url --external:vm
+esbuild src/index.ts --bundle --outfile=dist/index.js --external:url --external:vm
 cp src/fletch.d.ts dist/fletch.d.ts
