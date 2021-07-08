31c8205dafd2b8076f6b04e5e64bdc009c322e06
Rui Costa <ruicosta042@gmail.com>
2021-07-09 00:52:34 +0100

    feat: include correct files in package

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
 rimraf dist
-rimraf cjs
 tsc --build tsconfig.build.json
 
-esbuild src/index.cjs --bundle --platform=node --outfile=dist/index.cjs \
- --external:url --external:vm --external:node-fetch --external:cheerio
-esbuild src/index.ts --bundle --platform=node --format=esm --outfile=dist/index.mjs \
-  --external:url --external:vm --external:node-fetch --external:cheerio
+esbuild src/index.cjs --bundle --platform=node --outfile=dist/index.cjs
+esbuild src/index.ts --bundle --platform=node --format=esm --outfile=dist/index.mjs
 
 cp src/fletch.d.ts dist/fletch.d.ts
-
-# node12 compatibility
-mkdir cjs && cp dist/index.cjs cjs/index.js
