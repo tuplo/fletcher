d3864386238ade266c2aa423f175239887805c5a
Rui Costa <ruicosta042@gmail.com>
2023-06-17 23:13:14 +0100

    chore: update deps, security fixes

--- sh/build.ts
+++ sh/build.ts
@@ -1,6 +1,8 @@
-import { $ } from "@tuplo/shell";
+import * as shell from "@tuplo/shell";
 
 async function main() {
+	const $ = shell.$({ verbose: true });
+
 	await $`rm -rf dist`;
 	await $`tsc --project tsconfig.build.json`;
 
