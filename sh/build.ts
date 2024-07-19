6a24a5fd863b3b9cef2ea02b4832be33cf424cc8
Rui Costa <ruicosta042@gmail.com>
2024-07-19 13:22:05 +0100

    fix: include header-generator data_files in package

--- sh/build.ts
+++ sh/build.ts
@@ -1,4 +1,24 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+
 import * as shell from "@tuplo/shell";
+
+async function copyHeadersOrderFiles() {
+	// eslint-disable-next-line unicorn/prefer-module
+	const headerGenPkg = require.resolve("header-generator");
+	const headerGenDir = path.dirname(headerGenPkg);
+
+	const srcDir = path.join(headerGenDir, "data_files");
+	const destDir = path.resolve("dist/data_files");
+	await fs.mkdir(destDir, { recursive: true });
+
+	const dir = await fs.opendir(srcDir);
+	for await (const dirent of dir) {
+		const src = path.join(srcDir, dirent.name);
+		const dest = path.join(destDir, dirent.name);
+		await fs.copyFile(src, dest);
+	}
+}
 
 async function main() {
 	const $ = shell.$({ verbose: true });
@@ -15,6 +35,8 @@ async function main() {
 
 	await $`cp src/fletcher.d.ts dist/fletcher.d.ts`;
 	await $`rm -rf dist/mocks`;
+
+	await copyHeadersOrderFiles();
 }
 
 main();
