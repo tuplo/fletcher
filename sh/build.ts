319e2bdba495288532a236f106292c1792753571
Rui Costa <ruicosta042@gmail.com>
2024-08-12 12:16:46 +0100

    feat: use tsup to bundle package

--- sh/build.ts
+++ sh/build.ts
@@ -5,19 +5,7 @@ async function main() {
 
 	await $`rm -rf dist`;
 	await $`tsc --project tsconfig.build.json`;
-
-	const flags = [
-		"--bundle",
-		"--platform=node",
-		"--external:puppeteer-core",
-		"--external:axios",
-		"--external:hpagent",
-	];
-
-	await $`esbuild src/cjs/index.cjs --outfile=dist/index.cjs ${flags}`;
-
-	const esmFlags = [...flags, "--inject:sh/cjs-shim.ts"];
-	await $`esbuild src/index.ts --format=esm --outfile=dist/index.mjs ${esmFlags}`;
+	await $`tsup src/index.ts`;
 
 	await $`cp src/fletcher.d.ts dist/fletcher.d.ts`;
 	await $`rm -rf dist/mocks`;
